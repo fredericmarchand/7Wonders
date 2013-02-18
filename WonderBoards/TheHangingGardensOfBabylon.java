b4cf8878c5c4317e86c1698fd555bd326ccdc0c2
FredericMarchand <fredericmarchand@cmail.carleton.ca>
2013-02-18 11:43:55 -0500

    WonderBoard classes

---
+++ WonderBoards/TheHangingGardensOfBabylon.java
@@ -0,0 +1,23 @@
+package WonderBoards;
+
+public class TheHangingGardensOfBabylon extends WonderBoard{
+
+	public TheHangingGardensOfBabylon()
+	{
+		super(4, 1);
+	}
+	
+	public TheHangingGardensOfBabylon(int side)
+	{
+		super(4, side);
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ WonderBoards/TheLighthouseOfAlexandria.java
@@ -0,0 +1,23 @@
+package WonderBoards;
+
+public class TheLighthouseOfAlexandria extends WonderBoard {
+
+	public TheLighthouseOfAlexandria()
+	{
+		super(2, 1);
+	}
+	
+	public TheLighthouseOfAlexandria(int side)
+	{
+		super(2, side);
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
