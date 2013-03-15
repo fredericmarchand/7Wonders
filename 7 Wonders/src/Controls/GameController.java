d22e3b11d5c0671b9a8d1ae4cd2a4f2b229452a4
fredericmarchand <fredericmarchand@cmail.carleton.ca>
2013-03-15 18:17:19 -0400

    fuck3

--- 7 Wonders/src/Controls/GameController.java
+++ 7 Wonders/src/Controls/GameController.java
@@ -66,6 +66,7 @@ public class GameController implements Controller {
 			user.buildStructure(match.getLeftNeighbor(user), match.getRightNeighbor(user), 2);
 		}
 		user.getCards().remove(user.getChosenCard());
+		user.chooseCard(null);
 	}
 
 	@Override
@@ -85,6 +86,7 @@ public class GameController implements Controller {
 			user.buildStage(match.getLeftNeighbor(user), match.getRightNeighbor(user), 2);
 		}
 		user.getCards().remove(user.getChosenCard());
+		user.chooseCard(null);
 	}
 
 	@Override
@@ -171,6 +173,7 @@ public class GameController implements Controller {
 		// TODO Auto-generated method stub
 		user.discard(match.getDiscardedCards());
 		user.getCards().remove(user.getChosenCard());
+		user.chooseCard(null);
 	}
 
 	@Override
