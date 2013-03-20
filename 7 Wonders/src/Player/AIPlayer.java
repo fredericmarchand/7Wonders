3e1161928449b59d7a3f4c6dcea90168457de553
fredericmarchand <fredericmarchand@cmail.carleton.ca>
2013-03-19 23:27:47 -0400

    implemented random wonderboard sides, for AI, player tries to build the first buildable structure in hand, if none can be built, player tries to build a wonderstage with the first card in the hand, otherwise card is discarded

--- 7 Wonders/src/Player/AIPlayer.java
+++ 7 Wonders/src/Player/AIPlayer.java
@@ -13,38 +13,51 @@ public class AIPlayer extends Player {
 		isAI = true;
 	}
 	
-	public void pickCard(ArrayList<Structure> disca)
+	public void pickCard(ArrayList<Structure> discarded, Player leftNeighbor, Player rightNeighbor)
 	{
 		boolean did = false;
 		for ( int index = 0; index < cards.size(); ++index )
 		{
+			int result;
 			chooseCard(index);
-			if ( !wonderBoard.containsCard(chosenCard.getID()) )
+			result = canBuild(leftNeighbor, rightNeighbor);
+			switch ( result )
 			{
-				if ( ((chosenCard.getResourceCost().canAfford(Resources.addResources(extraResources, resources, unavailableResources)) 
-						|| chosenCard.canBuildForFree(wonderBoard)))  )
-				{
-					buildStructure();
-					did = true;
-					break;
-				}
+			case 0: continue;
+			
+			case 1: 
+				buildStructure();
+				did = true;
+				break;
+			case 2:
+				did = true;
+				buildStructure(leftNeighbor, rightNeighbor, 2);
+				break;
+			}
+		}
+		if ( !did )
+		{
+			chooseCard(0);
+			int result = canBuildStage(leftNeighbor, rightNeighbor);
+			switch ( result )
+			{
+			case 0: break;
+			
+			case 1: 
+				did = true;
+				buildStage(leftNeighbor, rightNeighbor, 2);
+				break;
+				
+			case 2:
+				did = true;
+				buildStage(leftNeighbor, rightNeighbor, 2);
+				break;
 			}
 		}
 		if ( !did )
 		{
 			getOwnedResources().addCoins(3);
 		}
-		/*chooseCard(0);
-		if ( !wonderBoard.containsCard(chosenCard.getID()) )
-		{
-			if ( ((chosenCard.getResourceCost().canAfford(Resources.addResources(extraResources, resources, unavailableResources)) 
-					|| chosenCard.canBuildForFree(wonderBoard)))  )
-				buildStructure();
-		}
-		else
-		{
-			getOwnedResources().addCoins(3);
-		}*/
 		cards.remove(chosenCardIndex);
 	}
 	
