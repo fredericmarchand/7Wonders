cca4eeaa9bb89bab676ebb255ad92eda11407d9d
Devon Ring <devon.ring@gmail.com>
2013-04-04 17:04:27 -0700

    more adjustment to server side match creation with AI players

--- 7 Wonders/src/Resources/Match.java
+++ 7 Wonders/src/Resources/Match.java
@@ -25,13 +25,14 @@ public class Match {
 	
 	ArrayList<Connection> connected;
 	ArrayList<User> userList;
-	ArrayList<User> aiList;
     ArrayList<CommandMessage> cmdMsgList;
     private long match_id;
     private static long counter = 1000;
     private int MAX_PLAYER_COUNT = 7 ;
     private int connection_count;
     private int receivedEvents = 0;
+    private int nHum;
+    private int nAI;
 
     private boolean inProgress;
     MServer server;
@@ -45,22 +46,24 @@ public class Match {
     private Match2 controller;
     
     public Match(int h,int ai, MServer m){
+    	nHum = h;
+    	nAI = ai;
     	server = m;
     	userList = new ArrayList<User>();
-    	aiList = new ArrayList<User>();
     	connected = new ArrayList<Connection>();
 		match_id = ++counter;
 		cmdMsgList = new ArrayList<CommandMessage>();
 		inProgress = false;
 		MAX_PLAYER_COUNT = h+ai;
-		generateAI(ai);			
+		connection_count+=ai;
+				
     }
     public int getMaxPlayerCount() {return MAX_PLAYER_COUNT;}
     public boolean get_inProgress(){return inProgress;}
 	public ArrayList<Connection> getConnections(){return connected;}	
 	public void addConnection(Connection c, Object o){
 		userList.add((User)o);		
-		connected.add(c);
+		connected.add(c);		
 		connection_count++;
 		update();
 	}
@@ -77,6 +80,7 @@ public class Match {
 	public long getMatch_ID(){return match_id;}
 	public int getConnectionCount(){return connection_count;}
 	public void update(){
+		//connection_count=connected.size();
 		System.out.println(connection_count);
 		if(connection_count==MAX_PLAYER_COUNT){
 			System.out.println("Starting Game!");
@@ -84,15 +88,14 @@ public class Match {
 		}
 	}
 
-	public void generateAI(int n){
+	public void generateAI(){
 		long idAI;
 		String usernameAI;
-		for(int i = 0; i<n;i++){
+		for(int i = 0; i<nAI;i++){
 			idAI= server.getID();
 			server.incID();
 			usernameAI = "JP the evil frenchmen"; 
-			aiList.add(controller.addAIPlayer(idAI, usernameAI));
-			connection_count++;
+			controller.addAIPlayer(idAI, usernameAI);
 			update();
 		}
 	}
@@ -117,17 +120,8 @@ public class Match {
 	public void receiveEvent(CommandMessage m, long cID){
 		cmdMsgList.add(m);
 		receivedEvents++;
-		if(receivedEvents==connection_count){
-			/***************************************/			
-
-			/***************************************/
-			//hand off info to game controller
-			//waiting
-			//okay game returned
-						
-			sendMatchInfo(controller.dispatch(cmdMsgList));
-			
-			
+		if(receivedEvents==connection_count){				
+			sendMatchInfo(controller.dispatch(cmdMsgList));			
 		}
 	}
 	
@@ -136,6 +130,7 @@ public class Match {
 	}	
 	public void startMatch(){
 		controller = new Match2(userList);
+		//generateAI(nAI);	
 		inProgress = true;
 		sendStartMatchRequest();
 	}
@@ -145,7 +140,7 @@ public class Match {
 	}
 	public void sendStartMatchRequest(){
 		Packet9StartMatch start = new Packet9StartMatch();
-		start.setObject(new Match2(userList));
+		start.setObject(controller);
 		
 			for(Connection c: connected){
 				c.sendTCP(start);
