ffb70fece88839e31dd558b39e2bb34fca040c40
Francis Lavoie <lavofr@gmail.com>
2013-04-08 22:05:36 -0400

    Added picking AI strategy

--- 7 Wonders/src/view/game/MatchPanel.java
+++ 7 Wonders/src/view/game/MatchPanel.java
@@ -212,25 +212,7 @@ public class MatchPanel extends JPanel implements Runnable {
 	}
 	
 	public void update() {
-		cardsPanel.setCards(match.getLocalPlayer().getCards());
-		if(match.getAge() == 4) {
-			lblAge.setIcon(null);
-			lblAge.setText("GAME OVER!");
-			lblTurn.setText(" ");
-		} else {
-			lblAge.setIcon(Images.get("age"+match.getAge()));
-			lblAge.setText(" ");
-			lblTurn.setText("Round "+ match.getTurn() +" of 6");
-		}
-		playerPanel.update();
-		n1.update();
-		n2.update();
-		if(f1 != null) f1.update();
-		if(f2 != null) f2.update();
-		if(f3 != null) f3.update();
-		if(f4 != null) f4.update();
-		fbp.update();
-		cardsPanel.update();
+		updateValues();
 		
 		// Choose resources
 		needResources = controller.needToChooseResources();
